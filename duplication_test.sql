c6e713ca0f4086e5205ee8c2618d1defd94ad796
Chris <claeubli@hotmail.com>
2025-08-31 20:16:27 +0200

    filter_for_pk_constraint

--- duplication_test.sql
+++ duplication_test.sql
@@ -25,6 +25,7 @@ FOR record IN (SELECT * FROM UNNEST(datasets) AS dataset)
             STRING_AGG(column_name, ', ')         AS primary_key_field,
             CONCAT(table_schema, '.', table_name) AS table_path
         FROM `{{params.destination_project}}.%s.INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE`
+        WHERE SPLIT(constraint_name, '.')[OFFSET(1)] = 'pk$'
         GROUP BY ALL
     """, record.dataset);
     END FOR;
