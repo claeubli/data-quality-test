979c0014db5bd78f9547e993c876e70927c94e8f
Christoph Laeubli <christoph.laeubli@justeattakeaway.com>
2026-02-23 21:52:44 +0100

    introduce_procedure

--- freshness_test.sql
+++ freshness_test.sql
@@ -1,54 +1,27 @@
-DECLARE projects ARRAY<STRING> DEFAULT ['dataset_1', 'dataset_2'];
-DECLARE delete_date DATE DEFAULT DATE_TRUNC(
-        DATE_SUB(CURRENT_DATE, INTERVAL CAST('{{params.date_range_months}}' AS INT) MONTH), YEAR);
+CREATE OR REPLACE PROCEDURE `{{params.destination_project}}`.`{{ params.destination_dataset }}.data_freshness`(
+    datasets ARRAY<STRING>,
+    temporary_table_name STRING
+)
+    OPTIONS ( DESCRIPTION = "This procedure will create a temporary table containing the records for each table in a dataset that wasn't modified in the last 24 hours" )
 
-CREATE TABLE IF NOT EXISTS `{{params.destination_dataset}}.freshness`
+BEGIN
+EXECUTE IMMEDIATE FORMAT("""
+CREATE OR REPLACE TEMPORARY TABLE `%s`
 (
-    test_datetime       DATETIME DEFAULT CURRENT_DATETIME('CET') NOT NULL OPTIONS (DESCRIPTION = 'This is the datetime when the freshness test was executed'),
-    test_name           STRING   NOT NULL OPTIONS (DESCRIPTION = 'This is the name of the data quality test'),
-    table_path          STRING   NOT NULL OPTIONS (DESCRIPTION = 'This is the affected table path'),
-    last_modified_datetime DATETIME NOT NULL OPTIONS (DESCRIPTION = 'This is the last datetime the metadata of the table was modified'))
-    PARTITION BY DATE(test_datetime)
-    OPTIONS (DESCRIPTION = "Contains records of all tables that weren't refreshed in the last 2 days",
-        LABELS = [("table_type", "data_quality"), ("time_zone", "local_timezone")]);
-
-CREATE OR REPLACE TEMPORARY TABLE freshness_meta_data
-(
-    table_path        STRING,
-    last_modified_datetime DATETIME
+    table_path              STRING,
+    last_modified_datetime  DATETIME
 );
+""", temporary_table_name);
 
-FOR record IN (SELECT * FROM UNNEST(projects) AS project)
+FOR record IN (SELECT * FROM UNNEST(datasets) AS dataset)
     DO
         EXECUTE IMMEDIATE FORMAT("""
-        INSERT INTO freshness_meta_data
+        INSERT INTO %s
         SELECT
         CONCAT(project_id, '.', dataset_id, '.', table_id) AS table_path,
         DATETIME(TIMESTAMP_MILLIS(last_modified_time), 'CET') AS last_modified_datetime
         FROM `{{params.destination_project}}.%s.__TABLES__`
         WHERE TIMESTAMP_DIFF(CURRENT_TIMESTAMP, TIMESTAMP_MILLIS(last_modified_time), DAY) > 1;
-    """, record.project);
+    """, temporary_table_name, record.dataset);
     END FOR;
-
-BEGIN TRANSACTION;
-
-DELETE
-FROM `{{params.destination_dataset}}.freshness`
-WHERE DATE(test_datetime) < delete_date;
-
-INSERT INTO `{{params.destination_dataset}}.freshness`  (test_name,
-                                                         table_path,
-                                                         last_modified_datetime)
-
-SELECT
-    'freshness' AS test_name,
-    table_path,
-    last_modified_datetime
-FROM freshness_meta_data;
-
-ASSERT (SELECT IF(COUNT(1) > 0, FALSE, TRUE)
-        FROM freshness_meta_data
-        WHERE DATETIME_DIFF(CURRENT_DATETIME('CET'), last_modified_datetime, DAY) = 2)
-    AS 'Data freshness test failed: To see which tables were not refreshed in the last two days run the following query:\nSELECT * FROM `{{params.destination_dataset}}.freshness` ORDER BY last_modified_datetime DESC LIMIT 100';
-
-COMMIT TRANSACTION;
+END;
