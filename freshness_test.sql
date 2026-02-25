492478c4d6625a9d90b5dde160d38e5afe6bbb66
Christoph Laeubli <christoph.laeubli@justeattakeaway.com>
2026-02-25 20:55:54 +0100

    delete_filter

--- freshness_test.sql
+++ freshness_test.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE PROCEDURE `{{params.destination_project}}`.`{{ params.destinat
     datasets ARRAY<STRING>,
     temporary_table_name STRING
 )
-    OPTIONS ( DESCRIPTION = "This procedure will create a temporary table containing the records for each table in a dataset that wasn't modified in the last 24 hours" )
+    OPTIONS ( DESCRIPTION = "This procedure will create a temporary table containing one record for each table in a dataset" )
 
 BEGIN
 EXECUTE IMMEDIATE FORMAT("""
@@ -20,8 +20,7 @@ FOR record IN (SELECT * FROM UNNEST(datasets) AS dataset)
         SELECT
         CONCAT(project_id, '.', dataset_id, '.', table_id) AS table_path,
         DATETIME(TIMESTAMP_MILLIS(last_modified_time), 'CET') AS last_modified_datetime
-        FROM `{{params.destination_project}}.%s.__TABLES__`
-        WHERE TIMESTAMP_DIFF(CURRENT_TIMESTAMP, TIMESTAMP_MILLIS(last_modified_time), DAY) > 1;
+        FROM `{{params.destination_project}}.%s.__TABLES__`;
     """, temporary_table_name, record.dataset);
     END FOR;
 END;
