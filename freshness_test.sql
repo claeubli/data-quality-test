b2a30b8ed8e43742280200246413623d1302104c
Christoph Laeubli <christoph.laeubli@justeattakeaway.com>
2026-02-25 21:16:31 +0100

    add_project_name

--- freshness_test.sql
+++ freshness_test.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE `{{params.destination_project}}`.`{{ params.destination_dataset }}.data_freshness`(
-    datasets ARRAY<STRING>,
+    project_dataset ARRAY<STRING>,
     temporary_table_name STRING
 )
     OPTIONS ( DESCRIPTION = "This procedure will create a temporary table containing one record for each table in a dataset" )
@@ -13,14 +13,14 @@ CREATE OR REPLACE TEMPORARY TABLE `%s`
 );
 """, temporary_table_name);
 
-FOR record IN (SELECT * FROM UNNEST(datasets) AS dataset)
+FOR record IN (SELECT * FROM UNNEST(project_dataset) AS dataset)
     DO
         EXECUTE IMMEDIATE FORMAT("""
         INSERT INTO %s
         SELECT
         CONCAT(project_id, '.', dataset_id, '.', table_id) AS table_path,
         DATETIME(TIMESTAMP_MILLIS(last_modified_time), 'CET') AS last_modified_datetime
-        FROM `{{params.destination_project}}.%s.__TABLES__`;
+        FROM `%s.__TABLES__`;
     """, temporary_table_name, record.dataset);
     END FOR;
 END;
